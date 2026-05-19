a1c2f10070b8111243dc64d79dfbee654a0aa994
Planum <33904555+PlanumCL@users.noreply.github.com>
2026-05-19 21:13:03 +0200

    feat: add StaffView and UsersView components for user management module

---
+++ src/modules/users/StaffView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1>Staff</h1>
+    <!-- UsersStaffList -->
+    <ul>
+      <li>user_data</li>
+    </ul>
+  </div>
+</template>
---
+++ src/modules/users/UsersView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1>Users</h1>
+    <!-- UsersClientList -->
+    <ul>
+      <li>user_data</li>
+    </ul>
+  </div>
+</template>
