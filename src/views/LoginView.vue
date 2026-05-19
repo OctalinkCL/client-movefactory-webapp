81a167947bf71eaea344976376c8838cd12c0615
Planum <33904555+PlanumCL@users.noreply.github.com>
2026-05-19 18:35:18 +0200

    config: add icons

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import { Button } from "@/components/ui/button";
+import { Camera } from "@lucide/vue";
 </script>
 
 <template>
   <div class="flex min-h-screen items-center justify-center">
-    <Button variant="outline">Button</Button>
+    <Button variant="outline">Button <Camera /></Button>
   </div>
 </template>
