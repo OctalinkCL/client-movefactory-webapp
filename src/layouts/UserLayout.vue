ab3c59cb5880dec27d86eb84eb21ca66151acaed
Planum <33904555+PlanumCL@users.noreply.github.com>
2026-05-28 10:48:14 +0200

    start user dashboard

--- src/layouts/UserLayout.vue
+++ src/layouts/UserLayout.vue
@@ -4,11 +4,11 @@ import AppFooterUser from '@/components/layouts/AppFooterUser.vue';
 </script>
 
 <template>
-    <div id="user-layout">
+    <div id="user-layout" class="flex flex-col min-h-screen">
         <!-- Header -->
         <AppHeaderUser />
         <!-- Content -->
-        <main class="container mx-auto px-4 lg:px-0">
+        <main class="flex-1 container mx-auto px-4 py-6 lg:px-0">
             <router-view />
         </main>
         <!-- Footer -->
--- src/modules/dashboard/UserDashboard.vue
+++ src/modules/dashboard/UserDashboard.vue
@@ -1,11 +1,35 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import { useAuthStore } from '@/stores/auth';
+// Components
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from '@/components/ui/avatar'
+
+const { profile } = storeToRefs(useAuthStore());
+
+
+</script>
 <template>
-  <div>
-    <h3>user_dashboard</h3>
-    <ul>
-      <li v-for="n in 50" :key="n">
-        Iteration number: {{ n }}
-      </li>
-    </ul>
+  <div id="user-dashboard-page" class="grid gap-4 lg:gap-8">
+    <!-- Header -->
+    <div class="flex gap-3 items-center">
+      <Avatar class="size-12">
+        <AvatarImage v-if="profile?.avatar_url" :src="profile?.avatar_url" :alt="profile?.full_name" />
+        <AvatarFallback class="text-xl">{{ profile?.initials }}</AvatarFallback>
+      </Avatar>
+      <div>
+        <h6 class="text-sm text-muted-foreground">Hola, Bienvenid@</h6>
+        <h3 class="text-xl font-medium">{{ profile?.full_name }}</h3>
+      </div>
+    </div>
+
+    <!-- Content -->
+    <div>
+      Content_
+    </div>
+
   </div>
 </template>
